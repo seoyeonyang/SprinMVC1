b0ae35ecd352aa3edad2ed21b0751e14ae3b63e9
Seoyeon <acc8837@naver.com>
2023-01-27 17:41:54 +0900

    project init

---
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,10 @@
+package hello.servlet.web.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+    
+}
